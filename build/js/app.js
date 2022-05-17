8955b70cf6a04c34330ebfedb550905e3ae6d1b2
Enrique Tachiquin Castañeda <etc_america@hotmail.com>
2022-05-17 00:17:47 -0500

    penultimo commit

--- build/js/app.js
+++ build/js/app.js
@@ -17,8 +17,47 @@ function crearGaleria() {
   <source srcset="build/img/thumb/${i}.avif" type="image/avif">
   <source srcset="build/img/thumb/${i}.webp" type="image/webp">
   <img loading="lazy" width="200" height="300" src="build/img/thumb/${i}.jpg" alt="imagen galeria">
-		`
+		`;
+		imagen.onclick = function () {
+			mostrarImagen(i);
+		}
 
 		galeria.appendChild(imagen);
 	}
+}
+
+function mostrarImagen(id) {
+		const imagen = document.createElement('picture');
+		imagen.innerHTML= `
+  <source srcset="build/img/grande/${id}.avif" type="image/avif">
+  <source srcset="build/img/grande/${id}.webp" type="image/webp">
+  <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="imagen galeria">
+		`;
+  //crea el Overlay con la imagen
+	const overlay = document.createElement('DIV');
+	overlay.appendChild(imagen);
+	overlay.classList.add('overlay');
+	overlay.onclick = function(){
+	const body = document.querySelector('body');
+	body.classList.remove('fijar-body');
+		overlay.remove();
+	}
+
+	//boton para cerrar el modal
+	const cerrarModal = document.createElement('p');
+	cerrarModal.textContent= 'X';
+	cerrarModal.classList.add('btn-cerrar');
+	cerrarModal.onclick = function () {
+		
+	const body = document.querySelector('body');
+	body.classList.remove('fijar-body');
+		overlay.remove();
+	}
+
+	overlay.appendChild(cerrarModal);
+
+  //añadir al HTML
+	const body = document.querySelector('body');
+	body.appendChild(overlay);
+	body.classList.add('fijar-body');
 }
